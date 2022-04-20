61d9c68a9f3e9823d6cddea28b5c253efa14d027
Tao Lizarraga <tlizarragagrigoni@frba.utn.edu.ar>
2022-04-19 23:27:29 -0300

    subo ejercicio resuelto

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main ()
+{
+    char a;
+    while (a=getchar()) // acá me tira un warning por el -Wall, pero el programa funciona correctamente
+    {
+        if (isupper(a)) a = tolower(a);
+        else if (islower(a)) a = toupper(a);
+        if (!isdigit(a)) putchar (a);
+    }
+    return 0;
+}
